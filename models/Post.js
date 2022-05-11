3091a6d808d6c6972a0c223df0deed0275c386c1
thetryworks <natesmyth1@gmail.com>
2022-05-11 13:21:40 -0500

    fully up to date from module

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
 class Post extends Model {
     static upvote(body, models) {
       return models.Vote.create({
@@ -19,6 +20,16 @@ class Post extends Model {
               sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
               'vote_count'
             ]
+          ],
+          include: [
+            {
+              model: models.Comment,
+              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+              include: {
+                model: models.User,
+                attributes: ['username']
+              }
+            }
           ]
         });
       });
